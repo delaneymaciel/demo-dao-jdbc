518664a6096b547865ea252a16c6c8778a62e629
Delaney Maciel Borges <delaneymaciel@hotmail.com>
2021-05-12 22:15:58 -0300

    findByID implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -10,15 +10,17 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		//Department obj = new Department(1,"Books");
+		//System.out.println(obj);
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+	//	Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(seller);
+		//System.out.println(seller);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
 	}
 
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,13 +2,15 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Seller;
+
 
 public interface SellerDao {
 	
-	void insert(SellerDao obj);
-	void update(SellerDao obj);
+	void insert(Seller obj);
+	void update(Seller obj);
 	void deleteById(Integer id);
-	SellerDao findById(Integer id);
-	List<SellerDao>findAll();
+	Seller findById(Integer id);
+	List<Seller>findAll();
 
 }
