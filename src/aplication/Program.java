d089910ee97b4c5f4cdeeefd19806a8315e2091c
Delaney Maciel Borges <delaneymaciel@hotmail.com>
2021-05-12 21:21:00 -0300

    SellerDaoJDBC Factory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SellerDao findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SellerDao> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
