ffe8b9e808122547a13aa7307a6df5fe43753882
Delaney Maciel Borges <delaneymaciel@hotmail.com>
2021-05-17 21:48:28 -0300

    findAll implamentation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -29,6 +29,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n *** TEST 3 : Seller findAll ***");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}		
+		
 	}
 
 }
